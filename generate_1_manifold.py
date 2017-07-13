7cde4b3849c65b29e28fd55ac966f3e9ddb37e5a
Alappin <alappin@iMac-2.local>
2017-07-13 02:09:33 -0400

    alg v1.0'

--- generate_1_manifold.py
+++ generate_1_manifold.py
@@ -3,11 +3,12 @@ from point import *
 import heapq
 
 
-def assemble_heap(vertices):
+def assemble_heap(vertex, graph):
     heap = []
-    for v in vertices:
-        for u in vertices:
-            heapq.heappush(heap, DPair((v, u), distance(v.p, u.p)))
+    for v in graph.vertices:
+        if v != vertex:
+            heapq.heappush(heap, DPair((vertex, v), distance(vertex.p, v.p)))
+    return heap
 
 
 def generate_1_manifold(g=None):
@@ -15,9 +16,47 @@ def generate_1_manifold(g=None):
         print("generate_1_manifold: not point cloud given.")
         return None
 
-    heap = assemble_heap(g.vertices)
-
+    heap_list = {}
     degrees = {}
     for v in g.vertices:
+        heap_list[v] = assemble_heap(v, g)
         degrees[v] = len(g.adj_list[v])
 
+    # Ensure that each vertex gets 0 or 1 neighbor
+    for v in g.vertices:
+        if len(g.adj_list[v]) == 0:
+            found = False
+            i = 0
+            while not found:
+                i += 1
+                if i == len(g.vertices):
+                    found = True
+                    g.add_edge(v, heapq.heappop(heap_list[v]).pair[1])
+
+                q = heapq.heappop(heap_list[v]).pair[1]
+                if len(g.adj_list[q]) == 0:
+                    found = True
+                    g.add_edge(v, q)
+                heapq.heapify(heap_list[v])
+
+    # Reset the heaps
+    heap_list = {}
+    for v in g.vertices:
+        heap_list[v] = assemble_heap(v, g)
+
+    # Ensure that each vertex gets up to 2 neighbors exactly
+    for v in g.vertices:
+        if len(g.ad_list[v]) < 2:
+            found = False
+            i = 0
+            while not found:
+                q = heapq.heappop(heap_list[v]).pair[1]
+                if len(g.adj_list[q]) < 2:
+                    found = True
+                    g.add_edge(v, q)
+                heapq.heapify(heap_list[v])
+
+    # Sanity Check
+    for v in g.vertices:
+        if len(g.adj_list[v]) != 2:
+            print("ERROR")
--- graph.py
+++ graph.py
@@ -12,3 +12,11 @@ class Graph:
         self.vertices = []
         self.adj_list = defaultdict(set)
 
+    def add_edge(self, u: Vertex, v: Vertex):
+        if u not in self.vertices or v not in self.vertices:
+            print("graph.add_edge: invalid vertices given.")
+            return None
+
+        self.adj_list[u].add(v)
+        self.adj_list[v].add(u)
+
